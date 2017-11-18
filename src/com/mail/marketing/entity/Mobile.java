aaa426b8fea1484e18ada20d2c755d16d047ce22
tunglv9x <tunglv9x@gmail.com>
2017-11-18 09:48:48 +0700

    t

---
+++ src/com/mail/marketing/entity/Mobile.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mail.marketing.entity;
+
+/**
+ *
+ * @author PMDVCNTT
+ */
+public class Mobile {
+    public static final String TABLE_NAME = "TBL_MOBILE";
+    int id;
+    String mobile;
+    String createDate;
+    int status;
+    String note;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+    
+}
